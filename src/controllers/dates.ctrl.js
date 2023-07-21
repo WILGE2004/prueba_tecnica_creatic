983dabaf0436c51affaee9f222ff5c69044cb7b4
WILGE2004 <wilhelmgeney@gmail.com>
2023-07-21 18:23:28 -0500

    first commit

--- src/controllers/dates.ctrl.js
+++ src/controllers/dates.ctrl.js
@@ -1,86 +1,43 @@
+const moment = require("moment");
+const { addDays, setHours, setMinutes, formatISO } = require("date-fns");
+
 const Sale = require("../models/dates.model");
 const sendRes = require("../lib/sendRes");
 const ctrlSales = {};
 
-function padTo2Digits(num) {
-  return num.toString().padStart(2, "0");
-}
+ctrlSales.getDates = async (req, res) => {
+  const fechasDisponibles = [];
+  const dias = 24;
+  const horasPorDia = 1;
+  const horaInicio = 3;
 
-function formatDate(date) {
-  return [
-    padTo2Digits(date.getDate()),
-    padTo2Digits(date.getMonth() + 1),
-    date.getFullYear(),
-  ].join("-");
-}
-
-ctrlSales.newSale = async (req, res) => {
-  const date = formatDate(new Date());
-  const { productos, total } = req.body;
-  try {
-    const sale = new Sale({
-      fecha: date,
-      productos,
-      total,
-    });
-    await sale.save();
-    sendRes(res, 201, true, "Sale Created");
-  } catch (error) {
-    sendRes(res, 400, false, "Sale NO Created");
-  }
-};
-
-ctrlSales.getTodaySales = async (req, res) => {
-  const date = formatDate(new Date());
-  try {
-    const todaySales = await Sale.find({ fecha: date });
-    if (todaySales.length === 0) {
-      return sendRes(res, 404, false, "No Sales Found");
+  for (let dia = 0; dia <= dias; dia += 3) {
+    for (let hora = 0; hora < horasPorDia; hora++) {
+      const fecha = new Date();
+      fecha.setDate(fecha.getDate() + dia);
+      fecha.setHours(horaInicio);
+      fecha.setMinutes(0);
+      fecha.setSeconds(0);
+      fechasDisponibles.push(fecha.toISOString());
     }
-    res.json(todaySales);
-  } catch (error) {
-    sendRes(res, 400, false, "Error: No Sales Found");
   }
+
+  res.json(fechasDisponibles);
 };
 
-ctrlSales.getDateSales = async (req, res) => {
-  const { date } = req.params;
+// Endpoint POST /appointments/reserve
+// app.post('/appointments/reserve', (req, res) => {
+//   const { date } = req.body;
 
-  try {
-    const dateSales = await Sale.find({ fecha: date });
-    if (dateSales.length === 0) {
-      return sendRes(res, 404, false, "No Sales Found");
-    }
-    res.json(dateSales);
-  } catch (error) {
-    sendRes(res, 400, false, "Error: No Sales Found");
-  }
-};
-
-ctrlSales.deleteOneSale = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const sale = await Sale.findOneAndDelete({ _id: id });
-    if (!sale) {
-      return sendRes(res, 404, false, "Sale NOT Found");
-    }
-    sendRes(res, 200, true, "Sale Deleted");
-  } catch (error) {
-    sendRes(res, 400, false, "Error: Sale NOT Found");
-  }
-};
-
-ctrlSales.deleteDateSale = async (req, res) => {
-  const { date } = req.params;
-  try {
-    const sale = await Sale.deleteMany({ fecha: date });
-    if (sale.deletedCount === 0) {
-      return sendRes(res, 404, false, "Sales NOT Deleted: Not found");
-    }
-    sendRes(res, 200, true, "Sales Deleted Success");
-  } catch (error) {
-    sendRes(res, 400, false, "Error...");
-  }
-};
+//   // Verificar si la fecha está disponible
+//   const index = availableAppointments.indexOf(date);
+//   if (index !== -1) {
+//     // Reservar la cita eliminando la fecha de la lista de disponibles
+//     availableAppointments.splice(index, 1);
+//     res.status(200).json({ message: 'Cita reservada exitosamente.' });
+//   } else {
+//     res.status(404).json({ message: 'La cita solicitada no está disponible.' });
+//   }
+// });
 
 module.exports = ctrlSales;
--- src/routes/apis.routes.js
+++ src/routes/apis.routes.js
@@ -4,16 +4,6 @@ const ctrlSales = require("../controllers/dates.ctrl");
 
 // sales routers
 
-router.post("/new/sale", ctrlSales.newSale);
-
-router.get("/date-sales/:date", ctrlSales.getDateSales);
-
-router.get("/today-sales", ctrlSales.getTodaySales);
-
-router.delete("/one-sale/:id", ctrlSales.deleteOneSale);
-
-router.delete("/date-sale/:date", ctrlSales.deleteDateSale);
-
-// products routers
+router.get("/appointments", ctrlSales.getDates);
 
 module.exports = router;
